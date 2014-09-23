3d191ce2b2477635bc2aa4476762743efe6602b6
Arturas Pavilonis <arturas.pavilonis@gmail.com>
2014-09-23 23:23:16 +0300

    Pupil photo is now shown on Pupil Edit Popup Window. If no photo available - showing default 'noPhoto' image.

--- controllers/ViewController.java
+++ controllers/ViewController.java
@@ -1,6 +1,12 @@
 package lt.pavilonis.monpikas.server.controllers;
 
+import com.vaadin.data.Item;
 import com.vaadin.event.ItemClickEvent;
+import com.vaadin.server.ExternalResource;
+import com.vaadin.server.FileResource;
+import com.vaadin.server.Resource;
+import com.vaadin.server.VaadinService;
+import com.vaadin.ui.Image;
 import com.vaadin.ui.Notification;
 import com.vaadin.ui.TabSheet;
 import com.vaadin.ui.UI;
@@ -11,21 +17,29 @@ import lt.pavilonis.monpikas.server.views.DinnerEventListView;
 import lt.pavilonis.monpikas.server.views.PupilEditWindow;
 import lt.pavilonis.monpikas.server.views.PupilsListView;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.MessageSource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 
-import java.util.Locale;
+import java.io.File;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 import static com.vaadin.ui.Notification.Type.TRAY_NOTIFICATION;
 
 @Controller
 public class ViewController {
 
-   @Autowired
-   private PupilService pupilService;
+   @Value("${PupilEditWindow.PhotoBasePath}")
+   private String adbImageBasePath;
+
+   @Value("${Images.Extension}")
+   private String imageExtension;
+
+   @Value("${Images.DefaultPhotoPath}")
+   private String noPhotoPath;
 
    @Autowired
-   private MessageSource messageSource;
+   private PupilService pupilService;
 
    @Autowired
    private DinnerService dinnerService;
@@ -47,29 +61,47 @@ public class ViewController {
 
    private ItemClickEvent.ItemClickListener newPulilListTableClickListener() {
       return event -> {
+         Item item = event.getItem();
+         long id = (long) event.getItemId();
          if (event.isDoubleClick()) {
-            PupilEditWindow editView = new PupilEditWindow(
-                  event.getItem(),
-                  messageSource.getMessage(
-                        "PupilEditWindow.Caption",
-                        new Object[]{event.getItem().getItemProperty("firstName"), event.getItem().getItemProperty("lastName")},
-                        Locale.getDefault()
-                  )
-            );
+            PupilEditWindow editView = new PupilEditWindow(item, getImage(id));
             editView.addCloseButtonListener(closeBtnClick -> editView.close());
-            editView.addSaveButtonListener(saveBtnClick -> {
-               editView.commit();
-               PupilInfo info = new PupilInfo(
-                     (long) event.getItemId(),
-                     (boolean) event.getItem().getItemProperty("dinnerPermitted").getValue(),
-                     (String) event.getItem().getItemProperty("comment").getValue()
-               );
-               pupilService.saveOrUpdate(info);
-               editView.close();
-               Notification.show("Išsaugota", TRAY_NOTIFICATION);
-            });
+            editView.addSaveButtonListener(
+                  saveBtnClick -> {
+                     editView.commit();
+                     boolean permission = (boolean) item.getItemProperty("dinnerPermitted").getValue();
+                     String comment = (String) item.getItemProperty("comment").getValue();
+                     pupilService.saveOrUpdate(new PupilInfo(id, permission, comment));
+                     editView.close();
+                     Notification.show("Išsaugota", TRAY_NOTIFICATION);
+                  });
             UI.getCurrent().addWindow(editView);
          }
       };
+   }
+
+   private Image getImage(long id) {
+      String remoteImgUrl = adbImageBasePath + id + imageExtension;
+      Resource resource;
+      if (remoteImageExists(remoteImgUrl)) {
+         resource = new ExternalResource(remoteImgUrl);
+      } else {
+         String basePath = VaadinService.getCurrent().getBaseDirectory().getAbsolutePath();
+         resource = new FileResource(new File(basePath + File.separator + noPhotoPath));
+      }
+      return new Image("", resource);
+   }
+
+   private boolean remoteImageExists(String url) {
+      try {
+         URL u = new URL(url);
+         HttpURLConnection http = (HttpURLConnection) u.openConnection();
+         http.setInstanceFollowRedirects(false);
+         http.setRequestMethod("HEAD");
+         http.connect();
+         return (http.getResponseCode() == HttpURLConnection.HTTP_OK);
+      } catch (Exception e) {
+         return false;
+      }
    }
 }
