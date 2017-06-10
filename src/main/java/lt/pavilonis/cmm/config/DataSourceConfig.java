2cab1e4b30f7442e10b179703a9383e7f13d7c71
Arturas Pavilonis <arturas.pavilonis@gmail.com>
2017-06-10 10:09:34 +0300

    Finally make connection pool setting work. Add spring boot actuator dependency

--- src/main/java/lt/pavilonis/cmm/config/DataSourceConfig.java
+++ src/main/java/lt/pavilonis/cmm/config/DataSourceConfig.java
@@ -9,7 +9,6 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
 
@@ -44,7 +43,7 @@ public class DataSourceConfig {
       return DataSourceBuilder.create().build();
    }
 
-   @Bean("dataSourceSalto")
+   @Bean
    @ConfigurationProperties(prefix = "spring.datasource.salto")
    public DataSource dataSourceSalto() {
       return DataSourceBuilder.create().build();
