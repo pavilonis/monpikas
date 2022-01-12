a4299ea5b38b50a1cc405a8188e638a2b0b2f9c6
Arturas Pavilonis <arturas.pavilonis@gmail.com>
2022-01-13 00:28:49 +0200

    Clean SystemUser code

--- src/main/java/lt/pavilonis/monpikas/security/SystemUser.java
+++ src/main/java/lt/pavilonis/monpikas/security/SystemUser.java
@@ -1,14 +1,21 @@
 package lt.pavilonis.monpikas.security;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
 import lt.pavilonis.monpikas.common.Named;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@SuperBuilder
 public final class SystemUser extends Named<Long> implements UserDetails {
 
    @NotBlank
@@ -23,8 +30,6 @@ public final class SystemUser extends Named<Long> implements UserDetails {
    private boolean enabled;
    private List<Role> authorities = new ArrayList<>();
 
-   public SystemUser() {/**/}
-
    public SystemUser(Long id, String name, String username, String password, String email, boolean enabled) {
       setId(id);
       setName(name);
@@ -32,49 +37,6 @@ public final class SystemUser extends Named<Long> implements UserDetails {
       this.password = password;
       this.email = email;
       this.enabled = enabled;
-   }
-
-   @Override
-   public String getPassword() {
-      return password;
-   }
-
-   public String getEmail() {
-      return email;
-   }
-
-   public boolean getEnabled() {
-      return enabled;
-   }
-
-   public void setUsername(String username) {
-      this.username = username;
-   }
-
-   public void setPassword(String password) {
-      this.password = password;
-   }
-
-   public void setEmail(String email) {
-      this.email = email;
-   }
-
-   public void setEnabled(boolean enabled) {
-      this.enabled = enabled;
-   }
-
-   @Override
-   public Collection<Role> getAuthorities() {
-      return this.authorities;
-   }
-
-   public void setAuthorities(List<Role> authorities) {
-      this.authorities = authorities;
-   }
-
-   @Override
-   public String getUsername() {
-      return username;
    }
 
    @Override
